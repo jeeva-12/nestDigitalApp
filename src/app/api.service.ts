377d80914e4a7ecff7a0a0dc93e262276e1d007c
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-11-30 23:40:19 +0530

    third

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  fetchCourse = () =>
+  {
+    return this.http.get("https://mylinkurcodesapp.herokuapp.com/getcourses")
+  }
+  addCourse =(dataToSend:any) =>
+  {
+    return this.http.post("https://mylinkurcodesapp.herokuapp.com/addcourse",dataToSend)
+  }
+
+  fetchfrnd =()=>
+  {
+    return this.http.get("https://dummyapifriends.herokuapp.com/view")
+  }
+  addfrnd=(dataToSend:any)=>
+  {
+    return this.http.post("https://dummyapifriends.herokuapp.com/adddata",dataToSend)
+  }
 }
